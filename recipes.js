0606a1aaaab81cd749e9532d45cbb7e8609c0629
Demars Berhe <demarsberhe@Demarss-MBP.fios-router.home>
2022-04-14 13:25:00 -0400

    apply border to randomly selected recipes

--- recipes.js
+++ recipes.js
@@ -1,10 +1,11 @@
 let recipes = [];
 
 //factory function that returns an object
-function createRecipe(name, mealType){
+function createRecipe(name, mealType, recipeId){
   return {
     name: name,
     mealType: mealType,
+    recipeId: recipeId,
     mealResponse: function(){
       if(this.name[name.length-1]==='s' || this.name==='Salmon' || this.name==='Mac and Cheese'){
         return `I will have ${this.name} for ${this.mealType}!`;
@@ -14,19 +15,18 @@ function createRecipe(name, mealType){
       
     }
     
-    
   }
 }
 // recipe objects 
-let veganPancakes = createRecipe('Vegan Pancakes', 'Breakfast');
-let overnightOats = createRecipe('Overnight Oats', 'Breakfast');
-let veggieWrap = createRecipe('Veggie Wrap', 'Lunch');
-let pasta = createRecipe('Broccoli Pesto Pasta', 'Dinner');
-let salmon = createRecipe('Salmon', 'Dinner');
-let macAndCheese = createRecipe('Mac and Cheese', 'Dinner');
-let chickenStirFry = createRecipe('Chicken Stir Fry', 'Dinner');
-let oreoCheesecakeBars = createRecipe('Oreo Cheesecake Bars', 'Dessert');
-let oreoTruffles = createRecipe('Oreo Truffles', 'Dessert');
+let veganPancakes = createRecipe('Vegan Pancakes', 'Breakfast', 'vegan-pancakes-title');
+let overnightOats = createRecipe('Overnight Oats', 'Breakfast', 'overnight-oats-title');
+let veggieWrap = createRecipe('Veggie Wrap', 'Lunch', 'veggie-wrap');
+let pasta = createRecipe('Broccoli Pesto Pasta', 'Dinner', 'pasta-title');
+let salmon = createRecipe('Salmon', 'Dinner', 'salmon-title');
+let macAndCheese = createRecipe('Mac and Cheese', 'Dinner', 'mac-and-cheese-title');
+let chickenStirFry = createRecipe('Chicken Stir Fry', 'Dinner', 'chicken-stir-fry-title');
+let oreoCheesecakeBars = createRecipe('Oreo Cheesecake Bars', 'Dessert', 'oreo-cheesecake-bars-title');
+let oreoTruffles = createRecipe('Oreo Truffles', 'Dessert', 'oreo-truffles-title');
 
 // individual recipe objects have been pushed to the recipes array
 recipes.push(veganPancakes);
@@ -55,7 +55,7 @@ function setLink(recipe){
   } else if(recipe.mealType === 'Dinner'){
     document.getElementById('recipe-link').setAttribute('href', '#dinner');
   } else if(recipe.mealType === 'Dessert'){
-    document.getElementById('recipe-link').setAttribute('href', '#dessert');
+    document.getElementById('recipe-link').setAttribute('href', 'dessert.html');
   }
   
 }
@@ -65,4 +65,7 @@ document.getElementById('recipe-button').addEventListener("click", function(){
   document.getElementById('recipe-selected').innerHTML = selectedRecipe.mealResponse(); //this updates the '...' text currently on the site with the randomly selected recipe
   document.getElementById('recipe-link').style.display = 'block'; // this displays the recipe link below the button
   setLink(selectedRecipe); //the href attribute of the 'Go to recipe!' will update with the corresponding link
+  document.getElementById('recipe-link').addEventListener('click', function(){
+    document.getElementById(`${selectedRecipe.recipeId}`).classList.add('selected-border'); // gray border is applied to the recipe selected by the generator
+  })
 })

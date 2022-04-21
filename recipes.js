b9e1855fdcc4419e2845456ed486ed76864eb23c
Demars Berhe <demarsberhe@Demarss-MBP.fios-router.home>
2022-04-20 23:44:52 -0400

    changed background color of buttons to light blue

--- recipes.js
+++ recipes.js
@@ -39,6 +39,10 @@ recipes.push(chickenStirFry);
 recipes.push(oreoCheesecakeBars);
 recipes.push(oreoTruffles);
 
+//recipeId values for each recipe object
+const idValues = recipes.map(recipe=>{return recipe.recipeId});
+console.log(idValues);
+
 
 //function selects a random recipe from the array
 function generateRandomRecipe(arrayOfRecipes){
@@ -46,32 +50,88 @@ function generateRandomRecipe(arrayOfRecipes){
   let randomNum = Math.round(Math.random()* numOfRecipes);
   return arrayOfRecipes[randomNum];
 }
-//function returns the section link of the recipe based on meal type
+//function sets the href value for the 'Go to recipe!' text to the title of the randomly selected recipe
 function setLink(recipe){
-  if(recipe.mealType === 'Breakfast'){
-    document.getElementById('recipe-link').setAttribute('href', '#breakfast');
-  } else if(recipe.mealType === 'Lunch'){
-    document.getElementById('recipe-link').setAttribute('href', '#lunch');
-  } else if(recipe.mealType === 'Dinner'){
-    document.getElementById('recipe-link').setAttribute('href', '#dinner');
-  } else if(recipe.mealType === 'Dessert'){
-    document.getElementById('recipe-link').setAttribute('href', 'dessert.html');
+  if(recipe.mealType === "Dessert"){
+    document.getElementById('recipe-link').setAttribute('href',`dessert.html#${recipe.recipeId}`);
+    //console.log(recipe.recipeId.classList)
+    //document.getElementById(`${recipe.recipeId}`).classList.add('selected-border'); 
+  } else{
+    document.getElementById('recipe-link').setAttribute('href', `#${recipe.recipeId}`);
+    //document.getElementById(`${recipe.recipeId}`).classList.add('selected-border'); 
   }
   
 }
+/*function addBorder(recipe){
+  if(recipe.mealType === "Dessert"){
+    document.getElementById('recipe-link').setAttribute('href',`dessert.html#${recipe.recipeId}`);
+    //console.log(recipe.recipeId.classList)
+    //document.getElementById(`${recipe.recipeId}`).classList.add('selected-border'); 
+  } else{
+    document.getElementById('recipe-link').setAttribute('href', `#${recipe.recipeId}`);
+    //document.getElementById(`${recipe.recipeId}`).classList.add('selected-border'); 
+  }
+  
+}*/
 
-document.getElementById('recipe-button').addEventListener("click", function(){
-  /*document.getElementsByTagName('figcaption').forEach(element =>{
-    if(element.classList.includes('selected-border')){
-      element.classList.remove('selected-border');
-    }
-  })*/
+
+document.getElementById('recipe-button').addEventListener("click", () =>{
   let selectedRecipe = generateRandomRecipe(recipes);
   document.getElementById('recipe-selected').innerHTML = selectedRecipe.mealResponse(); //this updates the '...' text currently on the site with the randomly selected recipe
   document.getElementById('recipe-link').style.display = 'block'; // this displays the recipe link below the button
   setLink(selectedRecipe); //the href attribute of the 'Go to recipe!' will update with the corresponding link
-  document.getElementById('recipe-link').addEventListener('click', function(){
-    document.getElementById(`${selectedRecipe.recipeId}`).classList.add('selected-border'); // gray border is applied to the recipe selected by the generator
-  })
+  document.getElementById('recipe-button').style.display = 'none';
+  document.getElementById('try-again').style.display = 'inline';
+  document.getElementById(`${selectedRecipe.recipeId}`).classList.add('selected-border'); // gray border is applied to the recipe selected by the generator
+  //NOTE: this class is not being applied to recipes located on the dessert.html page
+  /*
+ 
+
+  //commented out code
+  //document.getElementById(`${selectedRecipe.recipeId}`).classList.add('selected-border'); // gray border is applied to the recipe selected by the generator
+  
+  //if(selectedRecipe.mealType === "Dessert"){
+    //window.location = "dessert.html";
+    
+
+  //} else{
+    //document.getElementById(`${selectedRecipe.recipeId}`).classList.add('selected-border'); // gray border is applied to the recipe selected by the generator
+  //}
+  /*let highlightedElements = document.getElementsByClassName('selected-border');
+  console.log(highlightedElements);
+  console.log(highlightedElements.length);
+  if(highlightedElements.length > 1){
+    console.log(highlightedElements[1].classList);
+    highlightedElements[1].classList.remove('selected-border');
+  }*/
+  //highlightedElements.forEach(element=>{
+    //element.classList.remove('selected-border');
+  //});
+
+  /*let includeClass = idValues.filter(element=>{
+   if(document.getElementById(element).hasClass('selected-border')){
+      return element;
+      
+    }
+  //});
+  includeClass.forEach(element=>{
+    document.getElementById(element).classList.remove('selected-border')
+
+  })*/
+
+ 
+    //if(document.getElementById(`"${id}"`).classList.includes('selected-border')){
+      //document.getElementById(`"${id}"`).classList.remove('selected-border')
+      //let idVal = 
+      //console.log(idVal);
+    //}
+  //document.getElementsByTagName('figcaption').forEach(element =>{
+    //if(element.classList.includes('selected-border')){
+      //element.classList.remove('selected-border');
+    //}
+  //}
+  
+ 
 
 })
+
